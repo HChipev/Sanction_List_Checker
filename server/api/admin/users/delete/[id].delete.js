9f4d7cd39171fb6b1b961ba51e07c3480cd78e77
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-03-23 10:39:46 +0200

    fixed delete users bug

--- server/api/admin/users/delete/[id].delete.js
+++ server/api/admin/users/delete/[id].delete.js
@@ -1,10 +1,15 @@
-import { createClient } from "@supabase/supabase-js";
-const supabase = createClient(
-  useRuntimeConfig().public.supabase.url,
-  useRuntimeConfig().private.supabase.key
-);
+// import { createClient } from "@supabase/supabase-js";
+// const supabase = createClient(
+//   useRuntimeConfig().public.supabase.url,
+//   useRuntimeConfig().private.supabase.key
+
+import { serverSupabaseServiceRole } from "#supabase/server";
+
+// );
 export default defineEventHandler(async (event) => {
   const { id } = event.context.params;
-  const { data, error } = await supabase.auth.admin.deleteUser(id);
+  const { data, error } = await serverSupabaseServiceRole(
+    event
+  ).auth.admin.deleteUser(id);
   return { data, error };
 });
