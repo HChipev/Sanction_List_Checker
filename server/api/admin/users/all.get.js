6056c41dd14b74fd1751b79d093df4c2e7a3dc8f
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-03-23 10:25:39 +0200

    testing user error

--- server/api/admin/users/all.get.js
+++ server/api/admin/users/all.get.js
@@ -1,10 +1,13 @@
-import { createClient } from "@supabase/supabase-js";
-const supabase = createClient(
-  useRuntimeConfig().public.supabase.url,
-  useRuntimeConfig().private.supabase.key
-);
+// import { serverSupabaseServiceRole } from "#supabase/server";
+// import { createClient } from "@supabase/supabase-js";
+// const supabase = createClient(
+//   useRuntimeConfig().public.supabase.url,
+//   useRuntimeConfig().private.supabase.key
+// );
 export default defineEventHandler(async (event) => {
-  const { data, error } = await supabase.auth.admin.listUsers();
+  const { data, error } = await serverSupabaseServiceRole(
+    event
+  ).auth.admin.listUsers();
   data.users = data.users.filter(
     (user) => user.email !== useRuntimeConfig().public.admin
   );
